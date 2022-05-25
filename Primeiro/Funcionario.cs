5e17ae03c5c0eb4be531c039f4e8c38a6306794c
099865631 <099865631@IBM-PE08H46X>
2022-05-25 09:21:51 -0300

    Add project files.

---
+++ Primeiro/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeiro
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto =- Imposto;
+        }
+        public void AumentarSalario()
+        {
+            SalarioBruto =+ Imposto;
+        }
+
+    }
+}
